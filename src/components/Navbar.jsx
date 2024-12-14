ea08eaf12d4011c2854649f4473373af29253da9
malascoding <asepabdulmugni22@gmail.com>
2024-12-14 23:31:56 +0700

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 export default function Navbar() {
+  const [page, setPage] = useState("");
+
+  useEffect(() => {
+    setPage(document.title);
+  }, []);
   return (
     <>
       <nav className="flex h-20 w-full fixed fixed-top px-5 bg-slate-800 shadow-md">
@@ -8,16 +14,34 @@ export default function Navbar() {
           <div className=" w-2/12 h-full flex items-center font-bold text-white text-2xl">
             <Link to="/">AAM</Link>
           </div>
-          <ul className="h-full w-8/12 flex items-center justify-center gap-5 text-white font-semibold text-xl">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
+          <ul className="h-full w-8/12 flex items-center justify-center text-white font-semibold text-xl">
+            {page == "Home" ? (
+              <li className="bg-red-50 py-1 px-5 transition-all rounded-lg text-slate-800">
+                <Link to="/">Home</Link>
+              </li>
+            ) : (
+              <li className=" py-1 px-5 transition-all rounded-lg">
+                <Link to="/">Home</Link>
+              </li>
+            )}
+            {page == "About" ? (
+              <li className="bg-red-50 py-1 px-5 transition-all rounded-lg text-slate-800">
+                <Link to="/about">About</Link>
+              </li>
+            ) : (
+              <li className=" py-1 px-5 transition-all rounded-lg">
+                <Link to="/about">About</Link>
+              </li>
+            )}
+            {page == "Blog" || page == "Blog Detail" ? (
+              <li className="bg-red-50 py-1 px-5 transition-all rounded-lg text-slate-800">
+                <Link to="/blog">Blog</Link>
+              </li>
+            ) : (
+              <li className=" py-1 px-5 transition-all rounded-lg">
+                <Link to="/blog">Blog</Link>
+              </li>
+            )}
           </ul>
           <div className=" w-2/12 h-full flex justify-end items-center font-bold text-xl">
             <Link
